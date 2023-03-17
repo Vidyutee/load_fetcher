b79280b7dfa3a2ef347413ea96f55a98d47f4ee1
HridayAg0102 <hridayagrawal0102@gmail.com>
2023-03-17 22:10:37 +0530

    testing

---
+++ main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
+import pandas as pd
+
+def main():
+    html_text = requests.get('https://www.iexindia.com/marketdata/market_snapshot.aspx').text
+    soup = BeautifulSoup(html_text, 'lxml')
+
+    data = soup.find_all('td')
+
+    df = []
+    for i in range(96, 887):
+        df.append(data[i].text)
+
+    df = list(filter(lambda x: len(x) > 2, df))
+
+    df = [[df[i] for i in range(j, j+7)] for j in range(0, len(df), 7)]
+    headers = ["Time Block", "Purchase Bid (MW)", "Sell Bid (MW)", "MCV (MW)", "Final Scheduled Volume (MW)", "MCP (Rs/MWh)*", "Weighted MCP (Rs/MWh)"]
+
+    df = pd.DataFrame(np.vstack(df), columns = headers)
+    df = df.loc[:, ["Time Block", "MCP (Rs/MWh)*"]]
+    df.to_csv("data.csv", index = False)
+
+if __name__ == "__main__":
+    main()
