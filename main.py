62bcf58cbe651b7ea328713847ddc91d0787d515
Hriday Agrawal <76818035+HridayAg0102@users.noreply.github.com>
2023-03-21 11:26:52 +0530

    updated the data retrieved

--- main.py
+++ main.py
@@ -16,10 +16,14 @@ def main():
     df = list(filter(lambda x: len(x) > 2, df))
 
     df = [[df[i] for i in range(j, j+7)] for j in range(0, len(df), 7)]
-    headers = ["Time Block", "Purchase Bid (MW)", "Sell Bid (MW)", "MCV (MW)", "Final Scheduled Volume (MW)", "MCP (Rs/MWh)*", "Weighted MCP (Rs/MWh)"]
+    headers = ["Time Block", "Purchase Bid (MW)", "Sell Bid (MW)", "MCV (MW)", "Final Scheduled Volume (MW)", "MCP (Rs/kWh)*", "Weighted MCP (Rs/MWh)"]
 
     df = pd.DataFrame(np.vstack(df), columns = headers)
-    df = df.loc[:, ["Time Block", "MCP (Rs/MWh)*"]]
+    df = df.loc[:, ["Time Block", "MCP (Rs/kWh)*"]]
+    
+    # Orginally, the data retrieved has its MCP in Rs/MWh, which needs to be converted to Rs/kWh 
+    # by dividing by 1000
+    df["MCP (Rs/kWh)*"] = df["MCP (Rs/kWh)*"]/1000
 
     with open("data.csv",'w') as data_file:
         data_file.write(df.to_csv(index=False))
